578bd3807b335b35fc8b6f0b9b7703e9f4ba5d22
sadia-mehjabin <jhidnicraft@gmail.com>
2022-09-29 14:19:57 +0600

    card added

--- src/components/activities/Activities.js
+++ src/components/activities/Activities.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './Activities.css'
 
 const Activities = (props) => {
+    // const {handleAddToCart} = props;
     const {picture, time, name, description} = props.activity ;
+    
     return (
-        <div>
-            <img src={picture} alt="" />
+        <div className='activity'>
+            <img src={picture} alt=""></img> 
+            <p>Time Duration: {time}</p>
+            <p className='activity-name'>Name: {name}</p>
+            <p>Description: {description}</p>
+            <button onClick={() => props.handleAddToCart(props.activity)} className='btn-add-to-list'>Add To List</button>
         </div>
     );
 };
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -4,17 +4,23 @@ import './Layout.css'
 
 const Layout = () => {
     const [activities, setActivities] = useState([]);
+    
     useEffect(() => {
         fetch('generated.json')
         .then(res => res.json())
         .then(data => setActivities(data))
     },[])
+
+    const addToCartHandler = (activity) => {
+        console.log(activity)
+    }
     return (
         <div className='layout'>
             <div className="image-container">
                 {activities.map(activity => <Activities 
                 key={activity._id}
                 activity={activity}
+                handleAddToCart = {addToCartHandler}
                 ></Activities>)}
             </div>
             <div className="information">
